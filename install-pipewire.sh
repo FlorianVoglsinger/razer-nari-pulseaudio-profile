e639555245f47a15f8d5b1c0fa3fcb1636692f15
FlorianVoglsinger <129788409+FlorianVoglsinger@users.noreply.github.com>
2024-12-08 16:51:49 +0100

    Update install-pipewire.sh to fit kubuntu

--- install-pipewire.sh
+++ install-pipewire.sh
@@ -2,4 +2,4 @@
 cp razer-nari-input.conf /usr/share/alsa-card-profile/mixer/paths/
 cp razer-nari-output-{game,chat}.conf /usr/share/alsa-card-profile/mixer/paths/
 cp razer-nari-usb-audio.conf /usr/share/alsa-card-profile/mixer/profile-sets/
-cp 91-pulseaudio-razer-nari.rules.pipewire /lib/udev/rules.d/91-pulseaudio-razer-nari.rules
+cp 91-pulseaudio-razer-nari.rules.pipewire /lib/udev/rules.d/91-pipewire-razer-nari.rules
